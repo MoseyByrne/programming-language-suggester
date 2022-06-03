26a441baebac8929b7900bc7702e2b058c3c1f5e
MoseyByrne <mosey.b.byrne@gmail.com>
2022-06-03 11:57:35 -0700

    add button and branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
-$("form#quizForm").submit(function(event) {
-  event.preventDefault();
+$(document).ready(function() {
+  $("form#quizForm").submit(function(event) {
+    event.preventDefault();
 
+const beverageInput = $("input:radio[name=beverage]:checked").val();
+const pizzaInput = $("input:radio[name=pizza]:checked").val();
+const awakeInput = $("input:radio[name=awake]:checked").val();
+const petInput = $("input:radio[name=pet]:checked").val();
+const plantsInput = $("input:radio[name=plants]:checked").val();
 
-const beverage = $("input:radio[name=beverage]:checked").val();
-const pizza = $("input:radio[name=pizza]:checked").val();
-const awake = $("input:radio[name=awake]:checked").val();
-const pet = $("input:radio[name=pet]:checked").val();
-const plants = $("input:radio[name=plants]:checked").val();
+if (beverageInput === 'Water' || petInput === 'Cat') {
+  $('.output').text('C# is the language for you!');
+} else if (beverageInput === 'Coffee' || petInput === 'Dog') {
+  $('.output').text('Time for you to learn Ruby!');
+} else {
+  $('.output').text('Get ready to have some fun with JavaScript!');
+}
+
+  });
+});
