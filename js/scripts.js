309e24ea9b8c2cc261b5ed4bd05d40e194e91e9b
MoseyByrne <mosey.b.byrne@gmail.com>
2022-06-03 10:33:06 -0700

    clean up syntax in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+$("form#quizForm").submit(function(event) {
+  event.preventDefault();
+
+
 const beverage = $("input:radio[name=beverage]:checked").val();
 const pizza = $("input:radio[name=pizza]:checked").val();
 const awake = $("input:radio[name=awake]:checked").val();
