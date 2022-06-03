1d0cdd8e358ebd6bd76cd94e7713b33c50924c9a
MoseyByrne <mosey.b.byrne@gmail.com>
2022-06-03 09:51:39 -0700

    WIP add code to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-
+const beverage = $("input:radio[name=beverage]:checked").val();
+const pizza = $("input:radio[name=pizza]:checked").val();
+const awake = $("input:radio[name=awake]:checked").val();
+const pet = $("input:radio[name=pet]:checked").val();
+const plants = $("input:radio[name=plants]:checked").val();
